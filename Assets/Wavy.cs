520eef9328d23235324c48d0f8e0762228fe0045
Shawn Scribner <shawnwick@hotmail.com>
2018-01-18 17:32:12 -0600

    more stuff

---
+++ Assets/Wavy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wavy : MonoBehaviour {
+
+	public float smooth = 20;
+
+	// Use this for initialization
+	void Start () 
+	{   
+		SwingOpen ();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void SwingOpen()
+	{   
+		Quaternion newRotation = new Quaternion(transform.rotation.x,transform.rotation.y,transform.rotation.z,transform.rotation.w);;
+		newRotation *= Quaternion.Euler(0, 90, 0); // this add a 90 degrees Y rotation
+		transform.rotation= Quaternion.Slerp(transform.rotation, newRotation,20 * Time.deltaTime);      
+
+	}
+
+}
